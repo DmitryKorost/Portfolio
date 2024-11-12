6b2b58b4a8f7052f1c6b4b8a37f883da71920cfb
DmitryKorost <102684821+DmitryKorost@users.noreply.github.com>
2024-11-12 08:11:40 +0400

    Update App.js
    111

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Fon from './image/Монитор.jpg';
 function App() {
   return (
     <div>
-        <img src={Fon} className="App-logo" align="right" alt ="not image"/>
+        <img src={Fon} className="App-logo" align="right" alt="not image"/>
     </div>
   );
 }
